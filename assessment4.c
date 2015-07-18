1133dd1f3a6ea831e32923a36818a16177e9ab6a
Paul-Johan Seim <pjseim@gmail.com>
2015-07-18 18:07:24 -0400

    Update assessment4.c

--- assessment4.c
+++ assessment4.c
@@ -1,8 +1,9 @@
+# Copyright Paul-Johan Seim
+
 #include <stdio.h>
 
 main()
 {
-
     float e, eron, n;   
   
     eron = 1.0;
@@ -10,24 +11,11 @@ main()
       
     for (n = 1; n < 10; ++n)
     {
-
 	eron = eron * (1/n);
         e = e + eron;                              
       
     }
-
     
     printf("e = %6.8f\n", e);
-
-
-
-
-
-
-
-
-
-
-    
 
 }
